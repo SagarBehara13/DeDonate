a54b6e6ab815d8ed6b63944ee525614191cd4536
SagarBehara13 <sagar@cryptocontrol.io>
2020-10-11 16:02:12 +0530

    updated code

--- src/components/explore.js
+++ src/components/explore.js
@@ -23,6 +23,24 @@ const Explore = () => {
       {/*</div>*/}
       <div className="row">
       <div className="col-12">
+      <Nav tabs>
+        <NavItem>
+          <NavLink type="button"
+            className={classnames({ active: activeTab === '1' })}
+            onClick={() => { toggle('1'); }}
+          >
+            Peer To Peer
+          </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink type="button"
+              className={classnames({ active: activeTab === '2' })}
+              onClick={() => { toggle('2'); }}
+            >
+              Charity
+            </NavLink>
+          </NavItem>
+        </Nav>
         <TabContent activeTab={activeTab}>
           <TabPane tabId="1">
             <div className="row tabs-main-container">
@@ -162,6 +180,27 @@ const Explore = () => {
                 </div>
               </div>
             </Col>
+            </div>
+          </TabPane>
+          <TabPane tabId="2">
+            <div className="row tabs-main-container">
+              <Col sm="12" md="6">
+                <div className="custom-card">
+                  <div className="header-card">
+                    <img width="130px" height="130px" src={logo} alt="Card cap" className="card-image"/>
+                    <div className="header-card-content">
+                      <p className="name">Special Title Treatment</p>
+                      <p className="price">Card subtitle</p>
+                      <Button color="info" className="donate-btn">
+                        {/*<i className="fa fa-usd fa-lg"></i> */}
+                        Donate Now</Button>
+                    </div>
+                  </div>
+                    <div className="body-card">
+                      <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    </div>
+                </div>
+              </Col>
             </div>
           </TabPane>
         </TabContent>
--- src/components/request.js
+++ src/components/request.js
@@ -8,36 +8,51 @@ class Request extends Component {
        <div className="" id="request">
         <h4 className="form-head">Are you talented & Looking for donations?</h4>
         <h5 className="form-subhead">Provide us your details and let us give you the chance to meet your donors!!</h5>
-        <Form onSubmit={this.handleLogin} className="main-form">
-            <FormGroup>
-                <Label htmlFor="name" className="form-label">Full Name</Label>
-                <Input type="text" id="name" name="name"
-                    innerRef={(input) => this.name = input} placeholder="Enter your name here"
-                />
-            </FormGroup>
-            <FormGroup>
-              <Label htmlFor="criteria" className="form-label">Criteria</Label>
-              <Input type="select" name="criteria" id="criteria"
-                    innerRef={(input) => this.criteria = input}>
-                <option>Education</option>
-                <option>Sports</option>
-                <option>Others</option>
-              </Input>
-            </FormGroup>
-            <FormGroup>
-              <Label htmlFor="story" className="form-label">Tell us about yourself</Label>
-              <Input type="textarea" rows={3} columns={50} name="story" id="story"
-                    innerRef={(input) => this.story = input} placeholder="Describe in short why you need this" maxLength={200}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label htmlFor="exampleFile" className="form-label">Image Url</Label>
-              <Input type="text" name="photo" id="image" />
-              <FormText color="muted">
-                Upload your image posted on any social media
-              </FormText>
-            </FormGroup>
-            <Button className="form-btn" type="submit" value="submit" color="primary">Submit</Button>
+        <Form onSubmit={(event) => {
+          event.preventDefault()
+          const name = this.requestName.value
+          const price = window.web3.utils.toWei(this.requestPrice.value.toString(), 'Ether') || 0
+          const category = this.requestCategory.value
+          const story = this.requestStory.value
+          const image = this.requestImage.value
+          console.log(name, price, category, story, image);
+          this.props.createRequest(name, price, category, story, image)
+        }} className="main-form">
+          <FormGroup>
+            <Label htmlFor="name" className="form-label">Full Name</Label>
+            <Input type="text" id="requestName" name="name"
+                innerRef={(input) => this.requestName = input} placeholder="Enter your name here"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="criteria" className="form-label">Criteria</Label>
+            <Input type="select" name="criteria" id="requestCategory"
+                innerRef={(input) => this.requestCategory = input}>
+              <option>Education</option>
+              <option>Sports</option>
+              <option>Others</option>
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="story" className="form-label">Tell us about yourself</Label>
+            <Input type="textarea" rows={3} columns={50} name="story" id="requestStory"
+                  innerRef={(input) => this.requestStory = input} placeholder="Describe in short why you need this" maxLength={200}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="story" className="form-label">Donation Amount</Label>
+            <Input type="text" rows={3} columns={50} name="story" id="requestPrice"
+              innerRef={(input) => this.requestPrice = input} placeholder="amount"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="exampleFile" className="form-label">Image Url</Label>
+            <Input type="text" name="photo" id="requestImage" innerRef={(input) => this.requestImage = input} placeholder="image"/>
+            <FormText color="muted">
+              Upload your image posted on any social media
+            </FormText>
+          </FormGroup>
+          <Button className="form-btn" type="submit" value="submit" color="primary">Submit</Button>
         </Form>
       </div>
      </div>
